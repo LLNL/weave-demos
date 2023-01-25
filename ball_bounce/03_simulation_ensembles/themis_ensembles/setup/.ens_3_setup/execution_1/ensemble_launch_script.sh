adbcb75b866f56fede76195ebaaaf4c3201236cb
Sarah Eljurf <eljurf1@llnl.gov>
2023-01-25 10:14:36 -0800

    uq updates

--- ball_bounce/03_simulation_ensembles/themis_ensembles/setup/.ens_3_setup/execution_1/ensemble_launch_script.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-cd /g/g20/eljurf1/weave_demos/ball_bounce/03_simulation_ensembles/themis_ensembles/setup/.ens_3_setup/execution_1
-/g/g20/eljurf1/weave_demos/ball_bounce/ball_bounce_demo_venv/bin/python /g/g20/eljurf1/weave_demos/ball_bounce/ball_bounce_demo_venv/lib/python3.7/site-packages/themis/backend -t0 -a1 -p0 -e0 --setup-dir /g/g20/eljurf1/weave_demos/ball_bounce/03_simulation_ensembles/themis_ensembles/setup/.ens_3_setup -c 100 >> /g/g20/eljurf1/weave_demos/ball_bounce/03_simulation_ensembles/themis_ensembles/setup/.ens_3_setup/execution_1/themis_backend.log 2>&1
-wait
