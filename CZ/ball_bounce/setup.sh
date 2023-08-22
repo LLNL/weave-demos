5f8bc80b009d041350315653c36ba052421e8858
Jorge Moreno <moreno45@llnl.gov>
2023-08-22 06:43:31 -0700

    cleaning up

--- CZ/ball_bounce/setup.sh
+++ CZ/ball_bounce/setup.sh
@@ -4,7 +4,7 @@ python3 -m venv ball_bounce_demo_venv
 source ball_bounce_demo_venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-python -m ipykernel install --user --name ball-bounce-demo
+python3 -m ipykernel install --user --name ball-bounce-demo
 
 # Creating folders for data and images
 
@@ -15,7 +15,6 @@ mkdir ./01_baseline_simulation/num_res/images/
 mkdir ./03_simulation_ensembles/data/
 mkdir ./04_manage_data/data/
 mkdir ./05_post-process_data/images/
-mkdir ./06_surrogate_model/data/
 mkdir ./06_surrogate_model/images/
 
 
