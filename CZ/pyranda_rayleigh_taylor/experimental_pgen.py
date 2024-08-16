d507da5e316a2e466eb64b33100edbbef2fdc392
Charles Doutriaux <doutriaux1@llnl.gov>
2024-08-15 17:27:49 -0700

    added sina to pyranda study
    edited plot_study and bayesian to get data from store
    
    plot_study.py creates ensemble as well

--- CZ/pyranda_rayleigh_taylor/experimental_pgen.py
+++ CZ/pyranda_rayleigh_taylor/experimental_pgen.py
@@ -1,21 +1,22 @@
 import numpy as np
 from maestrowf.datastructures.core import ParameterGenerator
 
-# Generate sample points for the parameters from normal distributions
-# for our fake experimental data
-Nexperiments = 10
-
-# Atwood distribution samples
-atwood = np.round(0.48 + 0.04 * np.random.standard_normal(Nexperiments), 3)
-
-# Velocity magnitude samples
-velocity = np.round(0.97 + 0.05 * np.random.standard_normal(Nexperiments), 3)
-
-# Get a list of ints for the simulation random seed
-simseed  = list(range(Nexperiments))
 
 
 def get_custom_generator(env, **kwargs):
+    # Generate sample points for the parameters from normal distributions
+    # for our fake experimental data
+    Nexperiments = int(kwargs.get("NEXPERIMENTS", env.find("NEXPERIMENTS").value))
+
+
+    # Atwood distribution samples
+    atwood = np.round(0.48 + 0.04 * np.random.standard_normal(Nexperiments), 3)
+
+    # Velocity magnitude samples
+    velocity = np.round(0.97 + 0.05 * np.random.standard_normal(Nexperiments), 3)
+
+    # Get a list of ints for the simulation random seed
+    simseed  = list(range(Nexperiments))
 
     p_gen = ParameterGenerator()
 
